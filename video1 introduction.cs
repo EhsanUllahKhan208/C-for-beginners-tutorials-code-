21a141e1d40dc73b83b1c30486119be5892ab817
Ehsan Ullah Khan <165005220+EhsanUllahKhan208@users.noreply.github.com>
2024-04-05 09:06:20 +0500

    Add files via upload

---
+++ video1 introduction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to C# training");
+        Main1();
+    }
+
+    static void Main1()
+    {
+        Console.WriteLine("Welcome to C# training Ehsan");
+    }
+}
