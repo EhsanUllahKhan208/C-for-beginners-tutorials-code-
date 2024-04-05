a30bd897b9da8ed61a5995d5ce8d87a294e4dccc
Ehsan Ullah Khan <165005220+EhsanUllahKhan208@users.noreply.github.com>
2024-04-05 10:24:39 +0500

    Add files via upload

---
+++ video 4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace TrainingProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           String Name1 = "\"Ehsan\"";
+            String Name2 = "\\Ehsan";
+            String Name3 = @"\\\\\Ehsan";
+            Console.WriteLine(Name1);
+            Console.WriteLine(Name2);
+            Console.WriteLine(Name3);
+
+        }
+
+
+    }
+}
