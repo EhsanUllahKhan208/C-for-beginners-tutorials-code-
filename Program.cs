4edee7562dd537fa3f266fd13d14ded87a469c25
Ehsan Ullah Khan <165005220+EhsanUllahKhan208@users.noreply.github.com>
2024-04-06 10:04:30 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace video_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+
+            int result = num1 + num2;
+            Console.WriteLine("Addition result = {0}", result);
+
+            
+            int result1 = num1 - num2;
+            Console.WriteLine("Subtraction result = {0}", result1);
+
+          
+            int result2 = num1 * num2;
+            Console.WriteLine("Multiplication result = {0}", result2);
+
+            int result3 = num1 / num2;
+            Console.WriteLine("Division result = {0}", result3);
+
+            int result4 = num1 % num2;
+            Console.WriteLine("Reminder result = {0}", result4);
+            if (num1 == 10 || num2==20) {
+                Console.WriteLine("true");
+            }
+        }
+    }
+}
