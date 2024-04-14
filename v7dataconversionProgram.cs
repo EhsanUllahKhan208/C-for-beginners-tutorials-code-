67de607fc902cfe4bb6d67c5ae4c97dfccc5bbd3
Ehsan Ullah Khan <165005220+EhsanUllahKhan208@users.noreply.github.com>
2024-04-14 16:49:16 +0500

    Add files via upload

---
+++ v7dataconversionProgram.cs
@@ -0,0 +1,69 @@
+﻿namespace v7Datatypes_conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //implicit
+            int age1 = 25;
+
+            double pi = 3.14159; 
+
+            
+            double ageInYears = age1; 
+
+            Console.WriteLine(ageInYears); 
+
+            char initial = 'A';
+            int asciiValue = initial;
+
+            Console.WriteLine(asciiValue);
+
+            //explicit
+
+            double average = 89.75; 
+
+            
+            int roundedAverage = (int)average;
+
+            Console.WriteLine(roundedAverage); 
+
+          
+            string userInput = "1c";
+            int number;
+
+            bool success = int.TryParse(userInput, out number); // Attempt conversion
+
+            if (success)
+            {
+                Console.WriteLine("Converted number: " + number);
+            }
+            else
+            {
+                Console.WriteLine("Conversion failed: Input is not a valid number");
+            }
+
+            // str and num conversions
+            string ageStr = "30";
+            int age;
+            age = int.Parse(ageStr);
+
+            Console.WriteLine(age); 
+
+            string piStr = "3.14";
+            double piValue;
+
+            bool parseSuccess = double.TryParse(piStr, out piValue);
+
+            if (parseSuccess)
+            {
+                Console.WriteLine(piValue); 
+            }
+            else
+            {
+                Console.WriteLine("Conversion failed: Input is not a valid number");
+            }
+
+        }
+    }
+}
