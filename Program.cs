24d66634d184ad4952de2ec904efa0b3a1cae328
Ehsan Ullah Khan <165005220+EhsanUllahKhan208@users.noreply.github.com>
2024-04-14 16:50:39 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace v8Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[1000];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i * 2;  
+            }
+  
+            int specificNumber = numbers[50]; 
+
+            Console.WriteLine(specificNumber);
+
+            // fixed size
+            string[] names = new string[5]; 
+
+            names[0] = "Alice";
+            names[1] = "Bob";
+            names[2] = "Charlie";
+
+            Console.WriteLine(names.Length); 
+
+
+        }
+    }
+}
