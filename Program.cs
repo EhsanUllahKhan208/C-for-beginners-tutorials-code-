fa1b3660c758f7717a744fec35a318de049dcc04
Ehsan Ullah Khan <165005220+EhsanUllahKhan208@users.noreply.github.com>
2024-04-14 16:36:28 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace v6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 25; 
+
+            int? maybeAge; 
+
+            maybeAge = null;  
+
+            if (maybeAge.HasValue) 
+            {
+                Console.WriteLine("The age is: " + maybeAge.Value); 
+            }
+            else
+            {
+                Console.WriteLine("Age is unknown");
+            }
+
+        }
+    }
+}
