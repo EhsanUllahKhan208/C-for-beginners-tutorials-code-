b572a56343d20e75138820114f417d68508da57d
Ehsan Ullah Khan <165005220+EhsanUllahKhan208@users.noreply.github.com>
2024-04-15 17:07:05 +0500

    Add files via upload

---
+++ v11v12.cs
@@ -0,0 +1,49 @@
+﻿namespace Video11SwitchStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int grade = 88;
+            string letterGrade;
+
+            switch (grade)
+            {
+                case >= 90:
+                    letterGrade = "A";
+                    break;  
+
+                case >= 80:
+                    letterGrade = "B";
+                    break;  
+
+                case >= 70:
+                    letterGrade = "C";
+                    break; 
+
+                default:
+                    letterGrade = "F";
+                    break;
+            }
+
+            Console.WriteLine("Your letter grade is: " + letterGrade);
+
+           
+            int x = 5;
+
+        
+        startLoop:
+            Console.WriteLine("Value of x: " + x);
+
+            if (x > 0)
+            {
+                x--;
+                goto startLoop; 
+            }
+
+            Console.WriteLine("Loop finished (x is no longer positive)");
+        
+    }
+    }
+}
