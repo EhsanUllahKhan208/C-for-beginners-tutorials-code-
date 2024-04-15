941f543609e95161d110e423b6d4d9976a0af9e0
Ehsan Ullah Khan <165005220+EhsanUllahKhan208@users.noreply.github.com>
2024-04-15 17:34:47 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// MyMath.cs file in a MyCompany.MyApp namespace
+using MyCompany.MyApp;
+
+namespace MyCompany.MyApp
+{
+    public class MathHelper
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
+
+// MainProgram.cs file
+using MyCompany.MyApp;  // Using directive for MyCompany.MyApp namespace
+
+public class MainProgram
+{
+    public static void Main(string[] args)
+    {
+        int result = MathHelper.Add(5, 3);  // Accessing MathHelper.Add using namespace
+        Console.WriteLine("5 + 3 = " + result);
+    }
+}
+
