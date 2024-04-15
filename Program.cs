572b8d46f7ae038c0bf20fe70e06f90c194b795d
Ehsan Ullah Khan <165005220+EhsanUllahKhan208@users.noreply.github.com>
2024-04-15 17:33:19 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace v17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNumbers("Enter three numbers: ", 10, 20, 30);
+            PrintNumbers("Enter two numbers: ", 5, 15);
+        }
+        public static void PrintNumbers(string message, params int[] numbers)
+        {
+            Console.WriteLine(message);
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
