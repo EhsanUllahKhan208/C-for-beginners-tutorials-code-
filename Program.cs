0274769361a5dbdf58d369a604c7bc5ba8981d82
Ehsan Ullah Khan <165005220+EhsanUllahKhan208@users.noreply.github.com>
2024-04-15 17:18:17 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace v14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Secret number to guess
+            int secretNumber = 7;
+            int guess;
+
+            // do-while loop to keep prompting for guesses until the correct number is entered
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 10: ");
+                guess = int.Parse(Console.ReadLine()); // Read user input and convert to integer
+
+                if (guess == secretNumber)
+                {
+                    Console.WriteLine("Congratulations! You guessed the number.");
+                }
+                else
+                {
+                    Console.WriteLine("Try again. That wasn't the secret number.");
+                }
+
+            } while (guess != secretNumber); // Loop continues as long as guess is not equal to secretNumber
+        }
+    }
+}
